27f8c971d355bc478b8b4655d90f99440e1eadee
Abdullah Al-Ahmadi <abdu250@gmail.com>
2023-01-21 17:43:01 +0300

    Login completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,17 @@ import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { SettingsComponent } from './settings/settings.component';
 import {ShellModule} from "./shell/shell.module";
+import {MatCardModule} from "@angular/material/card";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import {MatIconModule} from "@angular/material/icon";
+import {ReactiveFormsModule} from "@angular/forms";
+import {FIREBASE_OPTIONS} from "@angular/fire/compat";
+import {MatSnackBarModule} from "@angular/material/snack-bar";
 
 @NgModule({
   declarations: [
@@ -20,9 +31,20 @@ import {ShellModule} from "./shell/shell.module";
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    ShellModule
+    ShellModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    MatIconModule,
+    ReactiveFormsModule,
+    MatSnackBarModule
   ],
-  providers: [],
+  providers: [
+    {provide: FIREBASE_OPTIONS, useValue: environment.firebase}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {AngularFireAuth} from "@angular/fire/compat/auth";
+import {Router} from "@angular/router";
+import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-login',
@@ -7,4 +11,27 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  loginForm: FormGroup
+  constructor(private fb:FormBuilder, private auth:AngularFireAuth, private router:Router, private alert:MatSnackBar) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.email, Validators.required]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    })
+  }
+
+  onLogin() {
+    const {email, password} = this.loginForm.value
+    this.auth.signInWithEmailAndPassword(email, password)
+      .then(result => {
+      this.router.navigateByUrl('dashboard')
+    }).catch(
+      error => {
+        this.alert.open(error.message, 'close', {
+          horizontalPosition: "center",
+          verticalPosition: "top"
+        })
+      }
+    )
+
+  }
 }
